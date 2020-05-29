fbf67809f52bae768ddd3968117fdbd0acb15201
int3l <int3l@users.noreply.github.com>
2020-05-30 01:58:56 +0300

    Twitch goes BRRRRRRRRRRRRR

--- unread_chat.user.js
+++ unread_chat.user.js
@@ -5,13 +5,14 @@
 // @description  make a highlight thingy so I know where I'm at in chat
 // @author       asottile
 // @match        https://dashboard.twitch.tv/popout/u/*/stream-manager/chat
+// @match        https://www.twitch.tv/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let highlighted = null, button = null;
+    let highlighted = null, button = null, chat_button = null;
 
     document.body.addEventListener('click', (e) => {
         let target = e.target;
@@ -27,11 +28,17 @@
         }
 
         if (button === null) {
-            button = document.createElement('button');
-            button.innerText = 'JUMP';
+            chat_button = document.querySelector('.tw-mg-l-05 > button')
+            if (chat_button === null) {
+                return
+            }
+
+            button = chat_button.cloneNode(true);
+            button.childNodes[0].childNodes[0].textContent = 'JUMP';
             button.addEventListener('click', () => {
                 highlighted.scrollIntoView();
             });
+
             let buttons = document.querySelector('[data-test-selector="chat-input-buttons-container"]');
             buttons.insertBefore(
                 button,
